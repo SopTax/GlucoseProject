e88a555a085391f12c4306bc7b53ddaad0efc422
SopTax <150542742+SopTax@users.noreply.github.com>
2023-11-12 17:08:49 +0100

    Add files via upload

---
+++ lstm_model.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+
+class SimpleLSTM(nn.Module):
+    # ... [SimpleLSTM class definition] ...
+  def __init__(
+      self,
+      input_size: int,
+      hidden_size:int,
+      num_layers:int,
+      output_size:int
+    ):
+    super().__init__()
+    self.hidden_size = hidden_size
+    self.num_layers = num_layers
+    self.batch_norm = nn.BatchNorm1d(input_size)
+    self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+    self.fc = nn.Linear(hidden_size, output_size)
+
+  def forward(self, x):
+    # Initialize hidden and cell states
+    h0 = t.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+    c0 = t.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+    # Forward propagate LSTM
+    out, _ = self.lstm(x, (h0, c0))  # out: tensor of shape (batch_size, seq_length, hidden_size)
+
+    # Decode the hidden state of the last time step
+    out = self.fc(out[:, -1, :])
+    return out
---
+++ training_function.py
@@ -0,0 +1,116 @@
+import torch as t
+from torch import nn
+from torch.utils.tensorboard import SummaryWriter
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+# Include the training function
+def train(net_class, input_size, hidden_size, num_layers, output_size, lr=0.01, num_epochs=100, batch_size=256):
+    # ... [train function definition] ...
+ train_dataloader = create_dataloader(
+      data_dirs=train_data_dirs,
+      seq_length=25,  # Example sequence length
+      batch_size=batch_size
+  )
+  test_dataloader = create_dataloader(
+      data_dirs=train_data_dirs,
+      seq_length=25,  # Example sequence length
+      batch_size=batch_size
+  )
+
+
+  model = net_class(input_size, hidden_size, num_layers, output_size)
+  print(model)
+
+  # Define loss function and optimizer
+  criterion = nn.MSELoss()  # for regression tasks; use nn.CrossEntropyLoss for classification tasks
+  optimizer = optim.Adam(model.parameters(), lr=lr)
+
+  train_losses: list[float] = []
+  test_losses: list[float] = []
+
+  best_validation_loss = float('inf')
+  best_model_dict = None
+
+  # Training loop
+  for epoch in range(num_epochs):
+
+      epoch_train_losses = []
+      model.train()  # Set model to training mode
+      for inputs, targets in train_dataloader:
+          # Zero the parameter gradients
+          optimizer.zero_grad()
+
+          # Forward pass
+          outputs = model(inputs)
+          loss = criterion(outputs, targets)
+
+          # Backward pass and optimize
+          loss.backward()
+          optimizer.step()
+          epoch_train_losses.append(loss)
+
+      if epoch == 80:
+        lr /= 10
+
+      # Validation phase
+      model.eval()  # Set model to evaluation mode
+      epoch_test_losses = []
+      with t.no_grad():  # No gradients required for validation
+          for inputs, targets in test_dataloader:
+              outputs = model(inputs)
+              # print(f'{outputs.shape=}')
+              # print(f'{targets.shape=}')
+              loss = criterion(outputs[:,-1], targets[:, -1])
+              epoch_test_losses.append(loss.item())
+
+      # Calculate average validation loss
+      # validation_loss /= len(validation_data_loader)
+      # Print statistics
+      #print(f'Epoch {epoch+1}/{num_epochs}, Validation Loss: {validation_loss:.4f}')
+      mean_train_loss = t.mean(t.tensor(epoch_train_losses))
+      mean_test_loss = t.mean(t.tensor(epoch_test_losses))
+      print(f'Epoch {epoch+1}/{num_epochs}\tTrain MSE: {mean_train_loss.item():.9f}\tTest MSE {mean_test_loss.item():.9f}')
+      # Store the loss value
+      train_losses.append(mean_train_loss.item())
+      test_losses.append(mean_test_loss.item())
+
+      writer.add_scalar('train loss', mean_train_loss, epoch)
+      writer.add_scalar('test loss', mean_test_loss, epoch)
+
+      current_validation_loss = mean_test_loss.item()
+      if current_validation_loss < best_validation_loss:
+          best_validation_loss = current_validation_loss
+          best_model_dict = model.state_dict()  # Save the model parameters
+
+
+  # Plot the loss values
+  plot_losses(train_losses, test_losses)
+  t.save(best_model_dict, 'best_model.pth')
+
+  assert best_model_dict is not None
+  model.load_state_dict(best_model_dict)
+
+  return train_losses, test_losses, model
+
+# Include the plot_losses function
+def plot_losses(train_losses, test_losses):
+    # ... [plot_losses function definition] ...
+    # Create a figure and a set of subplots
+    fig, axs = plt.subplots(2, 1, sharex=True)
+
+    # Plot for train losses
+    axs[0].plot(t.arange(len(train_losses[20:])), train_losses[20:], label='train')
+    axs[0].set_ylabel('Loss')
+    axs[0].legend()
+    axs[0].set_title('Train Loss')
+
+    # Plot for test losses
+    axs[1].plot(t.arange(len(test_losses[20:])), test_losses[20:], label='test')
+    axs[1].set_ylabel('Loss')
+    axs[1].legend()
+    axs[1].set_title('Test Loss')
+
+    # Set common x label
+    plt.xlabel('Epoch')
+    plt.show()
